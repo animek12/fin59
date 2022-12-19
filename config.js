84f57685b6a6f084b6daac861b80fae20e519d53
animek12 <111511065+animek12@users.noreply.github.com>
2022-12-20 07:58:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'Xient'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6281349622410','6281349622410','6281349622410']
+global.premium = ['6281349622410']
+global.packname = 'Xient'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
